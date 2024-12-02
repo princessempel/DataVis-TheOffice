b97023619e8f5b8783c60bc140d5c899ed343792
Patel, Aditi N <apatel768@gatech.edu>
2024-12-02 18:42:05 -0500

    removed the non-styled insights

--- dv-theoffice/src/App.js
+++ dv-theoffice/src/App.js
@@ -87,25 +87,6 @@ function App() {
           jsonFilePath={"/DataVis-TheOffice/data/character_data.json"}
         /> */}
       </div>
-      <div className="insights">
-        <header>
-          <h2>Insights from Profile Graphs</h2>
-        </header>
-        <p>
-          - After Michael's departure in Season 7, Episode 22, "Goodbye, Michael", we see that Dwight becomes the dialogue leader in Seasons
-          8 and 9, followed by some other characters like Andy, Oscar, and Kevin. <br />
-          - Jim and Pam has more steady presence in the series with relatively consistent dialogue and scene counts. Ocassionaly spikes in the lines numbers
-           and scene numbers for characters like Merdith in Season 5, Episode 11, "Moroccan Christmas", Oscar in Season 5, Episode 8, "Business Trip", and Toby
-          in Season 4, Episode 18/19, "Goodbye Toby" correlates with plotlines that focuses more on those characters. <br />
-          - Despite of everyone's contribution to Season 6, Episode 14, "The Banker" that episode has one of the worst ratings, as the fans sees it
-          as a "lazy" episode because majority of the episodes replays clips from other previous episode. And then Season 8, Episode 19, "Get the Girl" has a scaled rating of 0,
-          this is when Andy drives to Florida, to win Erin back while Nellie arrives at the Scranton Branch and siezes Andy's manager position, this could suggest that
-          Nillie is not a likeable character. <br />
-          - Two of the best episodes in the entire series Season 5, Episode 14, "Stress Relief" and Season 7, Episode 22, "Goodbye Michael" has contributions
-          from almost all the characters with Stanley having the highest lines in "Stress Relief" shows shows his meaningful and comedic contribution despite having less screentime overall.
-          Dwight having the highest lines in "Goodbye Michael" shows the depth of the relationship Michael and Dwight had along with how much the audience loved that duo. <br />
-        </p>
-      </div>
 
     </div>
   );
