a1048cb9aab132418708fcf624c63844f95c01be
sanjanadate <sanjanadate@gmail.com>
2024-12-03 12:12:52 -0500

    text analysis insights

--- dv-theoffice/src/TextAnalysis.js
+++ dv-theoffice/src/TextAnalysis.js
@@ -20,7 +20,13 @@ const TextAnalysis = () => {
     <div className="container">
       <div className="text">
         <h1 style={{flex: 1}}>Character Metrics Parallel Coordinates</h1>
-        <p>Description blurb?</p>
+            <ul>
+                <li style={{ color: "white" }}><b>Kelly</b> likes to gossip and often has emotional outbursts, which don’t require a sophisticated vocabulary. Her sentences are usually long because she tends to ramble without thinking ahead</li>
+                <li style={{ color: "white" }}><b>Meredith's</b> dialogue is often short, to the point, and laced with humor or shock value, rather than linguistic complexity.</li>
+                <li style={{ color: "white" }}><b>Dwight's</b> vocabulary often includes longer, more formal words because of his unique speech patterns.</li>
+                <li style={{ color: "white" }}><b>Oscar's</b> role as "the office intellectual" means his speech often includes more sophisticated</li>
+                <li style={{ color: "white" }}><b>Toby's</b> dialogue is subdued and repetitive and always speaks in a predictable language.</li>
+            </ul>
       </div>
       {data.length > 0 ? <ParallelCoordinates data={data} /> : <p>Loading...</p>}
     </div>
--- dv-theoffice/src/TextAnalysisHeatMap.js
+++ dv-theoffice/src/TextAnalysisHeatMap.js
@@ -21,7 +21,12 @@ const TextAnalysisHeatMap = () => {
     <div className='container'>
       <div className='text'>
         <h1>Character POS Heatmap</h1>
-        <p>Description/Insights</p>
+        <ul>
+                <li style={{ color: "white" }}><b>Erin's</b> bubbly and optimistic personality could lead her to use more descriptive language</li>
+                <li style={{ color: "white" }}><b>Phyllis's</b> use of verbs might reflect her tendency to narrate or engage in activities subtly</li>
+                <li style={{ color: "white" }}><b>Stanley</b> is known for his no-nonsense, practical, and often disinterested demeanor</li>
+                <li style={{ color: "white" }}><b>Meredith</b> is known for her straightforwardness while <b>Creed</b> often delivers cryptic one-liners or factual observations</li>
+            </ul>
       </div>
       {data.length > 0 ? <Heatmap data={data} /> : <p>Loading...</p>}
     </div>
